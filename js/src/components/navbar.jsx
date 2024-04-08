9631d3cdc986c8df9a5508a6d8673ff99ebdadca
BANADDA MUBARAKA <83466862+BANADDA@users.noreply.github.com>
2024-04-08 13:12:55 +0300

    modified:   js/src/components/navbar.jsx

--- js/src/components/navbar.jsx
+++ js/src/components/navbar.jsx
@@ -1,57 +1,99 @@
-import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
-import { Button, Collapse, IconButton, Navbar as MTNavbar, Typography, } from "@material-tailwind/react";
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
+import { Button, Collapse, IconButton, Navbar as MTNavbar, Typography } from '@material-tailwind/react';
 import Image from 'next/image';
-import React from "react";
+import React from 'react';
+
 function NavItem({ children, href, onClick }) {
-    return (<li>
-      <Typography as="a" href={href || "#"} onClick={onClick} // Attach onClick event
-     target={href ? "_blank" : "_self"} variant="small" className="font-bold">
+  return (
+    <li>
+      <Typography
+        as="a"
+        href={href || '#'}
+        onClick={onClick}
+        target={href ? '_blank' : '_self'}
+        variant="small"
+        className="font-bold"
+      >
         {children}
       </Typography>
-    </li>);
+    </li>
+  );
 }
+
 export function Navbar() {
-    const [open, setOpen] = React.useState(false);
-    const [isScrolling, setIsScrolling] = React.useState(false);
-    function handleOpen() {
-        setOpen((cur) => !cur);
+  const [open, setOpen] = React.useState(false);
+  const [isScrolling, setIsScrolling] = React.useState(false);
+
+  function handleOpen() {
+    setOpen((cur) => !cur);
+  }
+
+  React.useEffect(() => {
+    if (typeof window !== 'undefined') {
+      window.addEventListener('resize', () => {
+        if (window.innerWidth >= 960) {
+          setOpen(false);
+        }
+      });
+      window.addEventListener('scroll', handleScroll);
+      return () => {
+        window.removeEventListener('scroll', handleScroll);
+      };
     }
-    React.useEffect(() => {
-        window.addEventListener("resize", () => window.innerWidth >= 960 && setOpen(false));
-    }, []);
-    React.useEffect(() => {
-        function handleScroll() {
-            if (window.scrollY > 0) {
-                setIsScrolling(true);
-            }
-            else {
-                setIsScrolling(false);
-            }
-        }
-        window.addEventListener("scroll", handleScroll);
-        return () => window.removeEventListener("scroll", handleScroll);
-    }, []);
-    // Function to handle navigation to the "Who We Are" section
-    const navigateToWhoWeAre = () => {
-        const section = document.getElementById("about-us");
-        if (section) {
-            section.scrollIntoView({ behavior: "smooth" });
-        }
-    };
-    // Function to handle navigation to the "What We Do" section
-    const navigateToWhatWeDo = () => {
-        const section = document.getElementById("we-do");
-        if (section) {
-            section.scrollIntoView({ behavior: "smooth" });
-        }
-    };
-    return (<MTNavbar fullWidth shadow={false} blurred={false} color={isScrolling ? "white" : "transparent"} className="fixed top-0 z-50 border-0" style={{ backdropFilter: "blur(20px)" }}>
+  }, []);
+
+  function handleScroll() {
+    if (typeof window !== 'undefined') {
+      if (window.scrollY > 0) {
+        setIsScrolling(true);
+      } else {
+        setIsScrolling(false);
+      }
+    }
+  }
+
+  // Function to handle navigation to the "Who We Are" section
+  const navigateToWhoWeAre = () => {
+    const section = document.getElementById('about-us');
+    if (section) {
+      section.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
+
+  // Function to handle navigation to the "What We Do" section
+  const navigateToWhatWeDo = () => {
+    const section = document.getElementById('we-do');
+    if (section) {
+      section.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
+
+  return (
+    <MTNavbar
+      fullWidth
+      shadow={false}
+      blurred={false}
+      color={isScrolling ? 'white' : 'transparent'}
+      className="fixed top-0 z-50 border-0"
+      style={{ backdropFilter: 'blur(20px)' }}
+    >
       <div className="container mx-auto flex items-center justify-between">
-        <Typography as="a" href="" target="_blank" variant="h6" color={isScrolling ? "green" : "white"} className="flex items-center gap-2">
-          <Image src="/image/logo.png" alt="M-Vet Logo" width={35} height={35}/>
+        <Typography
+          as="a"
+          href=""
+          target="_blank"
+          variant="h6"
+          color={isScrolling ? 'green' : 'white'}
+          className="flex items-center gap-2"
+        >
+          <Image src="/image/logo.png" alt="M-Vet Logo" width={35} height={35} />
           M-Vet
         </Typography>
-        <ul className={`ml-10 hidden items-center gap-6 lg:flex ${isScrolling ? "text-green-900" : "text-white"}`}>
+        <ul
+          className={`ml-10 hidden items-center gap-6 lg:flex ${
+            isScrolling ? 'text-green-900' : 'text-white'
+          }`}
+        >
           <NavItem>Home</NavItem>
           {/* Modify the "Who We Are" NavItem */}
           <NavItem onClick={navigateToWhoWeAre}>Who We Are</NavItem>
@@ -59,17 +101,22 @@ export function Navbar() {
           <NavItem onClick={navigateToWhatWeDo}>What We Do</NavItem>
         </ul>
         <div className="hidden gap-2 lg:flex lg:items-end">
-          <IconButton variant="text" color={isScrolling ? "green" : "white"} size="sm"></IconButton>
-          <IconButton variant="text" color={isScrolling ? "green" : "white"} size="sm"></IconButton>
-          <IconButton variant="text" color={isScrolling ? "green" : "white"} size="sm"></IconButton>
+          <IconButton variant="text" color={isScrolling ? 'green' : 'white'} size="sm"></IconButton>
+          <IconButton variant="text" color={isScrolling ? 'green' : 'white'} size="sm"></IconButton>
+          <IconButton variant="text" color={isScrolling ? 'green' : 'white'} size="sm"></IconButton>
           <a href="">
-            <Button color={isScrolling ? "green" : "white"} size="sm">
+            <Button color={isScrolling ? 'green' : 'white'} size="sm">
               Contact Us
             </Button>
           </a>
         </div>
-        <IconButton variant="text" color={isScrolling ? "green" : "white"} onClick={handleOpen} className="ml-auto inline-block lg:hidden">
-          {open ? (<XMarkIcon strokeWidth={2} className="h-6 w-6"/>) : (<Bars3Icon strokeWidth={2} className="h-6 w-6"/>)}
+        <IconButton
+          variant="text"
+          color={isScrolling ? 'green' : 'white'}
+          onClick={handleOpen}
+          className="ml-auto inline-block lg:hidden"
+        >
+          {open ? <XMarkIcon strokeWidth={2} className="h-6 w-6" /> : <Bars3Icon strokeWidth={2} className="h-6 w-6" />}
         </IconButton>
       </div>
       <Collapse open={open}>
@@ -90,6 +137,8 @@ export function Navbar() {
           </div>
         </div>
       </Collapse>
-    </MTNavbar>);
+    </MTNavbar>
+  );
 }
+
 export default Navbar;
