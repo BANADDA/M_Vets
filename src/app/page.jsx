1b83303ac7a5323adc1c651049194e6e7621e71a
BANADDA MUBARAKA <83466862+BANADDA@users.noreply.github.com>
2024-04-08 13:49:57 +0300

    modified:   src/app/page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,26 +1,23 @@
-// components
+// Other imports remain the same
 import { Footer, Navbar } from "@/components";
-
-// sections
+import dynamic from 'next/dynamic';
 import Features from "./features";
 import Hero from "./hero";
 import Partners from "./partners";
-import VideoIntro from "./video-intro";
 import WhyChooseUs from "./why-choose-us";
+
+// Dynamically import VideoIntro with SSR disabled
+const VideoIntroWithNoSSR = dynamic(() => import('./video-intro'), { ssr: false });
 
 export default function Campaign() {
   return (
     <>
       <Navbar />
       <Hero />
-      <VideoIntro />
-      <Features/>
-      <Partners/>
-      <WhyChooseUs/>
-      {/* <Feature />
-      <MobileConvenience />
-      <Testimonials />
-      <Faqs /> */}
+      <VideoIntroWithNoSSR /> {/* Use the dynamically imported version */}
+      <Features />
+      <Partners />
+      <WhyChooseUs />
       <Footer />
     </>
   );
