d97314de9626aaf3c7e1b3813927b0f97707f7b7
BANADDA MUBARAKA <83466862+BANADDA@users.noreply.github.com>
2024-04-08 11:04:24 +0300

    modified:   src/app/faqs.tsx

--- src/app/faqs.tsx
+++ src/app/faqs.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import { Card, Typography } from "@material-tailwind/react";
 
 const FAQS = [
@@ -41,14 +39,16 @@ export function Faqs() {
             variant="lead"
             className="mx-auto mb-24 !text-gray-500 lg:w-3/5"
           >
-            A lot of people don&apos;t appreciate the moment until it&apos;s passed. I&apos;m not trying my hardest, and I&apos;m not trying to do
+            A lot of people don&apos;t appreciate the moment until it&apos;s
+            passed. I&apos;m not trying my hardest, and I&apos;m not trying to
+            do
           </Typography>
         </div>
 
         <div className="grid gap-20 md:grid-cols-1 lg:grid-cols-3">
           {FAQS.map(({ title, desc }) => (
             <Card key={title} shadow={false} color="transparent">
-              <Typography color="blue-gray" className="pb-6" variant="h4">
+              <Typography color="blue-gray" variant="h4">
                 {title}
               </Typography>
               <div className="pt-2">
